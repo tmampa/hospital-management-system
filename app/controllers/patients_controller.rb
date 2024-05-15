940745feaf5ef8431ee4b51965954abded21ae25
tmampa <tmampa94@gmail.com>
2024-05-15 14:20:57 +0200

    add other pages

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,6 +1,14 @@
 class PatientsController < ApplicationController
 	layout 'patient_dashboard'
 	def dashboard
+		@doctors = Doctor.all
+	end
 
+	def appointments
+		@appointments = current_patient.appointments
+	end
+
+	def treatments
+		# @treatments = current_patient.treatments
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :doctors, controllers: {
-    sessions: "doctors/sessions",
-    registrations: "doctors/registrations",
-  }
-  devise_for :patients, controllers: {
-    sessions: "patients/sessions",
-    registrations: "patients/registrations",
-  }
+	devise_for :doctors, controllers: {
+		sessions: "doctors/sessions",
+		registrations: "doctors/registrations",
+	}
+	devise_for :patients, controllers: {
+		sessions: "patients/sessions",
+		registrations: "patients/registrations",
+	}
 
+	# patient pages
+	get "/patients/dashboard", to: "patients#dashboard"
+    get "/patients/appointments", to: "patients#appointments"
+    get "/patients/treatments", to: "patients#treatments"
 
-  # patient pages
-    get "/patients/dashboard", to: "patients#dashboard"
+	# doctor pages
+	get "/doctors/dashboard", to: "doctors#dashboard"
 
-  # doctor pages
-    get "/doctors/dashboard", to: "doctors#dashboard"
-
-  root "home#index"
+	root "home#index"
 end
