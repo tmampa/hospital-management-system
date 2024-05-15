d9fdc1dcd5ecc83cf35910e45dd82360565dff26
tmampa <tmampa94@gmail.com>
2024-05-15 13:39:51 +0200

    direct to dashboard after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+	def after_sign_in_path_for(resource)
+		if resource.is_a?(Patient)
+			patients_dashboard_path
+		else
+			doctors_dashboard_path
+		end
+	end
 end
