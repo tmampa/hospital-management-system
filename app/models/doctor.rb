894f4476935ee905f6f9e7360e7ccdd2f993d1ff
tmampa <tmampa94@gmail.com>
2024-05-15 14:38:46 +0200

    display docs

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -6,4 +6,8 @@ class Doctor < ApplicationRecord
 
 	has_many :appointments, dependent: :destroy
 	has_many :patients, through: :appointments
+
+	def full_name
+		"#{first_name} #{last_name}"
+	end
 end
