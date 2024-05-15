a1dcf81a4ee450a4cf29beeedb5319825ca67827
tmampa <tmampa94@gmail.com>
2024-05-15 13:17:03 +0200

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :patients
+  devise_for :patients, controllers: {
+    sessions: "patients/sessions",
+    registrations: "patients/registrations",
+  }
   root "home#index"
 end
