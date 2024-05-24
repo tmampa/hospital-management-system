5f6728145943eb095c016f5a7118bc9438788bee
tmampa <tmampa94@gmail.com>
2024-05-24 05:15:43 +0200

    show payments

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,18 +1,20 @@
 class PatientsController < ApplicationController
-	layout 'patient_dashboard'
-	def dashboard
-		@doctors = Doctor.all
-	end
+  layout 'patient_dashboard'
 
-	def appointments
-		@appointments = current_patient.appointments
-	end
+  def dashboard
+    @doctors = Doctor.all
+  end
 
-	def treatments
-		@treatments = current_patient.treatments
-	end
+  def appointments
+    @appointments = current_patient.appointments
+  end
+
+  def treatments
+    @treatments = current_patient.treatments
+  end
 
   def payments
-		@payments = current_patient.payments
-	end
+    @patient = current_patient
+    @payments = @patient.payments
+  end
 end
